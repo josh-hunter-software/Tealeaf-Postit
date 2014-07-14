c2e487661577bb2fe8bee0dde30a1ea715f7dad3
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-07-14 15:50:37 -0500

    beginning_2nd_week

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   
   def index
     @posts = Post.all
 
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -29,12 +28,22 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated."
+      redirect_to posts_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end

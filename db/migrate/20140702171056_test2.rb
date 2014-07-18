ab7ed0b5e959a526a374b101e77ed1932f5e690b
Josh Hunter <thatsmegettinmail@yahoo.com>
2014-07-18 16:20:34 -0500

    final_commit

--- db/migrate/20140702171056_test2.rb
+++
@@ -1,7 +0,0 @@
-class Test2 < ActiveRecord::Migration
-  def change
-  	add_column :posts, :url, :string, default: nil
-  	add_column :posts, :title, :string, default: nil
-  	add_column :posts, :description, :text, default: nil
-  end
-end
